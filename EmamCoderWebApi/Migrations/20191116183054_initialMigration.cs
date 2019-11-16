a23ff8785b0dbe2b4d3a7eb64ea3e4b7fb843e5a
Emamul Hasan <emamcoder@outlook.com>
2019-11-17 01:28:05 +0530

    initialCommit

---
+++ EmamCoderWebApi/Migrations/20191116183054_initialMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmamCoderWebApi.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ EmamCoderWebApi/Model/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmamCoderWebApi.Model
+{
+    public class ProductDbContext: DbContext
+    {
+        public ProductDbContext(DbContextOptions option) : base(option)
+        {
+        }
+
+       public DbSet<Product> Products { get; set; }
+    }
+}
